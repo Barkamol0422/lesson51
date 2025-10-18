9b70b88f06a7cb4b09be2ef1b8a1bce23daae72d
Barkamol0422 <ganijonovbarkamol04222011@gmail.com>
2025-10-18 16:39:15 +0500

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,7 +1,13 @@
-a,b,c=map(int,input().split())
-d=a&b
-e=b|c
-f=b&c
-g=e&f
-h=d|g
-print(h)
+from math import *
+a=10
+b=99
+c=[]
+for i in range(a,b+1):
+    for j in range(2, int(sqrt(i))+1):
+        if i%j==0:
+            break
+        else:
+            c.append(i)
+print(*set(c))
+            
+        
